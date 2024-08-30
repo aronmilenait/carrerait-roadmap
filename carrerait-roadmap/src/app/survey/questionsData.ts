0ae79287e489910b2bcf12055615b7661f437686
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-08-30 08:23:43 -0300

    Add the beta version of the question's data.

---
+++ carrerait-roadmap/src/app/survey/questionsData.ts
@@ -0,0 +1,167 @@
+export const questionsData = [
+  {
+    question: "¿Estás trabajando?",
+    answers: [
+      {
+        text: "Actualmente, no trabajo.",
+        response:
+          "El hecho de que no trabajes, en el caso de no necesitarlo en el corto o mediano plazo, puede darte una ventaja a la hora de estudiar. Si tenés la posibilidad de que alguien te ayude en este momento, ¡mejor! Aunque sé que es algo que no todos tienen.",
+      },
+      {
+        text: "Sí, pero no se relaciona con IT.",
+        response:
+          "El hecho de que estés trabajando puede brindarte la posibilidad de financiar tu educación, ya sea de manera autodidacta, universitaria o una combinación de ambas, ¿por qué no?",
+      },
+      {
+        text: "Trabajo en IT, en el área que más me gusta.",
+        response:
+          "El hecho de que ya estés trabajando en el rubro Y en el área de tu preferencia te pone en una posición ventajosa. Estás ganando experiencia que va a servirte mucho en el futuro, y al mismo tiempo estás aprendiendo en un entorno laboral real. Además, te brinda la posibilidad de financiar tu educación, ya sea de manera autodidacta, universitaria o una combinación de ambas.",
+      },
+      {
+        text: "Trabajo en IT, pero no estoy en el área que quiero.",
+        response:
+          "El hecho de que estés trabajando en IT, aunque no sea en el área de tu preferencia, sigue siendo una ventaja, ya que lograste ingresar. Además, te brinda la posibilidad de financiar tu educación, ya sea de manera autodidacta, universitaria o una combinación de ambas.",
+      },
+    ],
+  },
+  {
+    question: "¿Qué pensás de la universidad?",
+    answers: [
+      {
+        text: "Me encanta.",
+        response:
+          "Te gusta mucho la universidad, por lo que esa posibilidad está sobre la mesa. Aunque recordá que aún así, vas a tener que investigar por tu cuenta y no quedarte con lo enseñado ahí. El rubro requiere de que estemos constantemente aprendiendo, y ahí entra en juego lo autodidacta.",
+      },
+      {
+        text: "Me da igual.",
+        response:
+          "La universidad te da igual. ¿Por qué? Ni idea. Si querés tener el título y una ventaja en el mercado actual, podrías considerarlo.",
+      },
+      {
+        text: "Me gusta, pero prefiero la modalidad virtual.",
+        response:
+          "Es entendible que te guste la modalidad virtual universitaria. Es una de las mejores opciones cuando estás trabajando, y aún si no lo estuvieras, te sirve para escuchar las clases desde la comodidad de tu casa, inclusive aprovechando para cocinarte algo o limpiar. En Argentina, hay varias universidades que ofrecen licenciaturas, ingenierías y tecnicaturas a distancia, como la Universidad Tecnológica Nacional (UTN), la Universidad de La Plata (UNLP), La Universidad Nacional de Siglo 21 (U21) y la Universidad de Palermo (UP), por mencionar algunos ejemplos.",
+      },
+      {
+        text: "Me gusta, pero prefiero la modalidad presencial.",
+        response:
+          "Es entendible que prefieras la modalidad presencial universitaria. Mucha gente considera que aprende mejor en esos entornos y, además, podés aprovechar y hacer networking. En Argentina, hay muchas universidades (son mayoría) que la tienen, como la Universidad de Buenos Aires (UBA), la Universidad Nacional de Hurlingham (UNA HUR), la Universidad Tecnológica Nacional (UTN) y la Universidad Nacional de Tres de Febrero (UNTREF), por mencionar algunos ejemplos.",
+      },
+      {
+        text: "La odio. No quiero estudiar en ese tipo de instituciones.",
+        response:
+          "No sé por qué odiás a la universidad, pero ahí no me meto. Vos tendrás tus razones, y son respetables. En ese caso, podrías considerar hacer un esfuerzo para formarte y obtener una ventaja en el mercado laboral actual, o enfocarte en tu formación autodidacta, que también es válida. Podés hacer cursos, proyectos personales y rendir certificaciones que avalen tus conocimientos.",
+      },
+    ],
+  },
+  {
+    question: "¿Cuántas horas por semana trabajás por lo general?",
+    answers: [
+      {
+        text: "Te dije que no trabajo.",
+        response: "Ninguna.",
+      },
+      {
+        text: "Entre 10 y 20hs semanales.",
+        response:
+          "Debido a que trabajás entre 10 y 20hs semanales, seguro tengas tiempo a tu disposición para dedicarle a tus estudios. Esta carga horaria es ideal para ir a la universidad, ya sea presencial y virtual, pero si no está en tus opciones, podés enfocarte en tu formación autodidacta.",
+      },
+      {
+        text: "Entre 20 y 25hs semanales.",
+        response:
+          "Debido a que trabajás entre 20 y 25hs semanales, seguro tengas algo de tiempo a tu disposición para dedicarle a tus estudios, aunque puede resultarte difícil la organización de tus horarios al principio. Podrías considerar ir a la universidad, ya sea presencial y virtual, pero si no está en tus opciones, podés enfocarte en tu formación autodidacta.",
+      },
+      {
+        text: "Entre 25 y 35hs semanales.",
+        response:
+          "Debido a que trabajás entre 25 y 35hs semanales, puede que se te dificulte ir a la universidad, ya sea virtual o presencial. En el caso de que quieras hacerlo, te recomiendo que te anotes en pocas materias y que lo lleves tranqui, a tu ritmo.",
+      },
+      {
+        text: "Entre 35 y 40hs semanales.",
+        response:
+          "Debido a que trabajás entre 35 y 50hs semanales, ir a la universidad va a ser un desafío, ya sea virtual o presencial. En el caso de que quieras hacerlo, te recomiendo que te anotes en 1 a 3 materias como máximo, y que lo lleves tranqui, a tu ritmo.",
+      },
+      {
+        text: "Más de 40hs semanales.",
+        response:
+          "Debido a que trabajás más de 40hs semanales, ir a la universidad va a ser un desafío, ya sea virtual o presencial, y seguramente te parezca imposible. En el caso de que quieras hacerlo, te recomiendo que te anotes en 1 a 2 materias como máximo, y que lo lleves tranqui, a tu ritmo. En el caso de ser posible, fijate de hablar con tus jefes y reducir tu jornada laboral en el caso de que quieras seguir estudiando (si está en tus posibilidades). Mientras tanto, puede ser que formarte de manera autodidacta sea la mejor opción.",
+      },
+    ],
+  },
+  {
+    question:
+      "¿Tenés alguna 'responsabilidad' que sentís que te impide seguir formándote?",
+    answers: [
+      {
+        text: "No, no tengo ninguna responsabilidad que me haga sentir así.",
+        response:
+          "Por otro lado, está bueno que no tengas responsabilidades que te impidan seguir estudiando (o que las tengas, pero que no lo veas como un inconveniente). Independientemente de lo que elijas, sabés que vas a poder hacerlo. (O que, al menos, estás dispuesto a intentarlo).",
+      },
+      {
+        text: "Tengo hijos (tranqui, no te vuelve mal padre/madre elegir esta opción)",
+        response:
+          "Tener hijos, sobre todo si son niños, puede dificultar un poco las cosas: llevarlos al jardín o al colegio, cocinarles, visitas al médico y bueno, todo lo que implica ser padre/madre. Lo bueno es que muchas personas logran estudiar teniendo esta responsabilidad. Si vas a ir a la universidad, quizás tengas que anotarte en menos materias, y si vas a ser autodidacta, es cuestión de que organices tus horarios de la forma que mejor te resulte.",
+      },
+      {
+        text: "Problemas personales o familiares.",
+        response:
+          "Los problemas personales y/o familiares pueden influir bastante a la hora de estudiar. Si estás en una situación que perjudica tu salud mental y/o física, afecta a todos los ámbitos de tu vida, incluyendo el académico. Mi recomendación es que busques la manera de solucionarlos, dependiendo de cuáles sean los inconvenientes. Por ejemplo: si no te sentís bien anímicamente, podrías considerar buscar ayuda profesional. Lo importante es que estés bien, y si eso implica dejar de lado tus estudios por un tiempo, ¡no te preocupes! Es parte del proceso. Espero que logres salir adelante.",
+      },
+      {
+        text: "No consigo organizarme.",
+        response:
+          "La falta de organización es un problema que muchos atraviesan. Todos tenemos una vida con distintas responsabilidades y obligaciones, y es importante tenerlas en cuenta a la hora de planificar. A su vez, organizarse es cuestión de prueba y error, de ver lo que funciona mejor para vos. Podés fallar al principio, pero lo importante es intentarlo y mejorar.",
+      },
+    ],
+  },
+  {
+    question: "¿Hiciste/hacés algún bootcamp?",
+    answers: [
+      {
+        text: "Sí, en el pasado.",
+        response:
+          "Dado a que hiciste un bootcamp en el pasado, asumo que tenés algunos conocimientos del área que te interesa. Sin embargo, hoy en día, muchos procesos de selección descartan a personas egresadas de los mismos. Te recomiendo que lo elimines de tu CV, Linkedln y portafolio, en caso de tenerlo. Ya fue.",
+      },
+      {
+        text: "No, nunca hice uno.",
+        response:
+          "La verdad, no hacer un bootcamp no es algo negativo. Podés aprender por tu cuenta, en la universidad o de la forma que consideres más apropiada.",
+      },
+      {
+        text: "Sí, estoy haciendo uno.",
+        response:
+          "Dado a que estás haciendo un bootcamp, asumo que tenés algunos conocimientos del área que te interesa. Sin embargo, hoy en día, muchos procesos de selección descartan a personas egresadas de los mismos. Te recomiendo que no lo pongas en tu CV, Linkedln y portafolio, en caso de tenerlo. Hacelo para aprender, pero después desechalo.",
+      },
+      {
+        text: "No, y no pienso hacer uno.",
+        response:
+          "Dado a que no pensás hacer un bootcamp, me parece una excelente decisión, ya que hoy en día, muchos procesos de selección descartan a personas egresadas de los mismos. Podés aprender por tu cuenta, en la universidad o de la forma que consideres más apropiada.",
+      },
+    ],
+  },
+  {
+    question: "¿Estás estudiando actualmente?",
+    answers: [
+      {
+        text: "Sí, estoy en la universidad.",
+        response:
+          "¡Qué bien! Recordá que, a pesar de estar estudiando una carrera universitaria, tenés que investigar por tu cuenta y no quedarte con lo enseñado ahí. El rubro requiere de que estemos constantemente aprendiendo, y ahí entra en juego lo autodidacta. Es genial que tengas la posibilidad de estudiar, y si no tenés otras responsabilidades, ¡aprovechalo al máximo!",
+      },
+      {
+        text: "No, no estoy estudiando nada.",
+        response:
+          "Ser autodidacta es una excelente opción en este rubro. Si bien no es para todos, es una habilidad que podés ir aprendiendo. Lo importante es que te pongas metas y objetivos, ya que te puede ayudar a organizarte. Si bien a veces se puede dificultar, sobre todo si tenés responsabilidades, es una excelente opción si sabés organizarte.",
+      },
+      {
+        text: "Sí, estoy haciendo cursos.",
+        response:
+          "¡Qué bien! Ser autodidacta es una excelente opción en este rubro. Si bien no es para todos, es una habilidad que podés ir aprendiendo. Lo importante es que te pongas metas y objetivos, ya que te puede ayudar a organizarte. Si bien a veces se puede dificultar, sobre todo si tenés responsabilidades, es una excelente opción si sabés organizarte. Recordá que, a pesar de estar haciendo cursos, tenés que investigar por tu cuenta y no quedarte con lo enseñado ahí.",
+      },
+      {
+        text: "Sí, estudio por mi cuenta.",
+        response:
+          "¡Qué bien! Ser autodidacta es una excelente opción en este rubro. Si bien no es para todos, es una habilidad que podés ir aprendiendo. Lo importante es que te pongas metas y objetivos, ya que te puede ayudar a organizarte. Si bien a veces se puede dificultar, sobre todo si tenés responsabilidades, es una excelente opción si sabés organizarte.",
+      },
+    ],
+  },
+];
