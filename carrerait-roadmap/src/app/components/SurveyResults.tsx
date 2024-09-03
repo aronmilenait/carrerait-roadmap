85022ccbdcd15e4ab7f83abb43e54e05534fa4f8
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-03 19:36:08 -0300

    Add more feedback to the SurveyResults component.

--- carrerait-roadmap/src/app/components/SurveyResults.tsx
+++ carrerait-roadmap/src/app/components/SurveyResults.tsx
@@ -10,7 +10,7 @@ const SurveyResults = ({ selectedAnswers, questions }: SurveyResultsProps) => {
   return (
     <div className="mt-8">
       <h2 className="text-3xl text-center font-bold mb-4 mt-8">Resultados</h2>
-      <div className="space-y-4">
+      <div className="flex flex-col">
         {selectedAnswers.map((answerIndex, questionIndex) => {
           if (answerIndex !== null) {
             return (
@@ -24,6 +24,31 @@ const SurveyResults = ({ selectedAnswers, questions }: SurveyResultsProps) => {
           }
           return null;
         })}
+      </div>
+      <div className="text-center mt-8 bg-sky-600 rounded-lg p-4">
+        <p className="text-white text-lg">
+          ¡Espero que estas respuestas te hayan sido útiles! <br />
+          En CarreraIT, también tenemos{" "}
+          <b>
+            <a href="/guias" className="font-bold hover:underline">
+              guías de orientación
+            </a>
+          </b>{" "}
+          para que puedas evaluar otras cuestiones e informarte. Además, podés
+          visitar nuestra sección de{" "}
+          <b>
+            <a href="/contacto" className="font-bold hover:underline">
+              contacto
+            </a>
+          </b>{" "}
+          si tenés alguna duda o sugerencia e{" "}
+          <b>
+            <a href="/informacion" className="font-bold hover:underline">
+              información
+            </a>
+          </b>{" "}
+          para conocer más acerca del proyecto.
+        </p>
       </div>
     </div>
   );
