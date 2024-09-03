793d1eb4a191407ba263ba5aed35969f74bf9116
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-03 20:15:24 -0300

    Add guides.ts file

---
+++ carrerait-roadmap/src/app/lib/guides.ts
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { Guide, GuideMeta } from "../types/types";
+import { remark } from "remark";
+import html from "remark-html";
+
+const guidesDirectory = path.join(process.cwd(), "content", "guides");
+
+export function getGuideSlugs(): string[] {
+  return fs
+    .readdirSync(guidesDirectory)
+    .map((file) => file.replace(/\.md$/, ""));
+}
+
+export function getGuideMeta(slug: string): GuideMeta {
+  const fullPath = path.join(guidesDirectory, `${slug}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const { data } = matter(fileContents);
+  return {
+    title: data.title,
+    description: data.description,
+  };
+}
+
+export async function getGuideContent(slug: string): Promise<Guide> {
+  const fullPath = path.join(guidesDirectory, `${slug}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  const { data, content } = matter(fileContents);
+
+  const processedContent = await remark().use(html).process(content);
+  const contentHtml = processedContent.toString();
+
+  return {
+    slug,
+    title: data.title,
+    description: data.description,
+    content: contentHtml,
+  };
+}
+
+export function getAllGuidesMeta(): GuideMeta[] {
+  const slugs = getGuideSlugs();
+  return slugs.map((slug) => getGuideMeta(slug));
+}
