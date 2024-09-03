16c50a4434fecbf04726256c39b48bdf42102e62
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-03 08:32:40 -0300

    Add the survey's page and link it in the navbar.

---
+++ carrerait-roadmap/src/app/cuestionario/page.tsx
@@ -0,0 +1,9 @@
+import Survey from "../components/Survey";
+
+export default function About() {
+  return (
+    <main className="bg-sky-950 text-white p-24">
+      <Survey />
+    </main>
+  );
+}
