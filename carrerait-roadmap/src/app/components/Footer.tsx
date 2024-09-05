91a8deabbfd19f120995825c0231d9b3f1920379
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-05 19:56:17 -0300

    Change the footer's background color.

--- carrerait-roadmap/src/app/components/Footer.tsx
+++ carrerait-roadmap/src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-gray-300 py-6 px-4 sm:px-6 md:px-8 lg:px-16">
+    <footer className="bg-teal-900 py-6 px-4 sm:px-6 md:px-8 lg:px-16">
       <div className="max-w-4xl mx-auto text-center">
         <a
           href="/"
