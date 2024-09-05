fa1f50a94ab05d2391e2fbb3c8551a43bcfdf5f0
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-04 21:11:27 -0300

    Refactor the footer component.

--- carrerait-roadmap/src/app/components/Footer.tsx
+++ carrerait-roadmap/src/app/components/Footer.tsx
@@ -1,42 +1,36 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-
 const Footer = () => {
   return (
-    <footer className="text-center p-6 bg-sky-800 text-sky-100">
-      <p className="mb-4">
-        <i>Carrera IT Roadmap</i> es desarrollado por Milena Sol Aron. <br />
-        Si tenés alguna pregunta o feedback, ¡no dudes en contactarme!
-      </p>
-      <div className="flex justify-center space-x-6">
+    <footer className="bg-gray-900 text-gray-300 py-6 px-4 sm:px-6 md:px-8 lg:px-16">
+      <div className="max-w-4xl mx-auto text-center">
         <a
-          href="https://github.com/aronmilenait"
-          className="text-sky-300 hover:text-sky-400"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="GitHub"
+          href="/"
+          className="text-4xl md:text-3xl font-extrabold text-teal-100 relative z-10"
         >
-          <FontAwesomeIcon icon={faGithub} className="h-6 w-6" />
+          <span className="text-teal-300">Carrera</span>
+          <span className="text-teal-500">IT</span>
         </a>
-        <a
-          href="https://www.linkedin.com/in/milenasaron"
-          className="text-sky-300 hover:text-sky-400"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="LinkedIn"
-        >
-          <FontAwesomeIcon icon={faLinkedin} className="h-6 w-6" />
-        </a>
-        <a
-          href="mailto:aronmilena.it@gmail.com"
-          className="text-sky-300 hover:text-sky-400"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Email"
-        >
-          <FontAwesomeIcon icon={faEnvelope} className="h-6 w-6" />
-        </a>
+        <nav className="mt-4 flex justify-center space-x-6">
+          <a
+            href="/"
+            className="font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300"
+          >
+            Inicio
+          </a>
+          <a
+            href="/cuestionario"
+            className="font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300"
+          >
+            Cuestionario
+          </a>
+          <a
+            href="https://www.linkedin.com/in/milenasaron"
+            className="font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contacto
+          </a>
+        </nav>
       </div>
     </footer>
   );
