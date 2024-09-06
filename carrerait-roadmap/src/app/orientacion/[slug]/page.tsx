11d8033c480d4fa7b527d3e98e375e6b4564aef4
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-05 21:05:52 -0300

    Make the slug's page responsive

--- carrerait-roadmap/src/app/orientacion/[slug]/page.tsx
+++ carrerait-roadmap/src/app/orientacion/[slug]/page.tsx
@@ -20,19 +20,16 @@ export default async function GuidePage({ params }: Params) {
   const guide: Guide = await getGuideContent(slug);
 
   return (
-    <main className="bg-gray-900 min-h-screen text-teal-900 p-6 md:p-12">
-      <article className="max-w-3xl mx-auto bg-teal-50 p-8 rounded-lg shadow-md">
-        <header className="mb-6">
-          <h1 className="text-4xl font-extrabold text-teal-900 mb-4">
+    <main className="bg-gray-900 min-h-screen text-teal-900 p-4 sm:p-6 md:p-12">
+      <article className="max-w-3xl mx-auto bg-teal-50 p-4 sm:p-6 md:p-8 rounded-lg shadow-md">
+        <header className="mb-4 sm:mb-6">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-teal-900 mb-2 sm:mb-4">
             {guide.title}
           </h1>
-          <p className="text-sky-800 text-lg">
-            Publicado el {new Date().toLocaleDateString()}
-          </p>
         </header>
         <section
           dangerouslySetInnerHTML={{ __html: guide.content }}
-          className="prose lg:prose-xl text-gray-950"
+          className="prose sm:prose lg:prose-lg xl:prose-xl text-gray-950"
         />
       </article>
     </main>
