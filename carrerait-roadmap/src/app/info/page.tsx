d7c566e3177d720589eb18ac559e8aa991acfa4b
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-06 07:59:43 -0300

    Change the order of the components in the info page.

--- carrerait-roadmap/src/app/info/page.tsx
+++ carrerait-roadmap/src/app/info/page.tsx
@@ -4,8 +4,8 @@ import AboutWebsite from "../components/AboutWebsite";
 export default function About() {
   return (
     <main className="p-6 md:p-24"> 
-      <AboutMe />
       <AboutWebsite />
+      <AboutMe />
     </main>
   );
 }
