da8071dedc9a3cf3a3e56ebef783a41caadfad6f
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-06 21:35:03 -0300

    Add resources data.

---
+++ carrerait-roadmap/src/app/data/resources.ts
@@ -0,0 +1,16 @@
+export const resources = [
+  {
+    title: "Open Source University (OSSU): Computer Science",
+    type: "course",
+    description:
+      "Estudiá ciencias de la computación en inglés de manera gratuita y autodidacta.",
+    link: "https://ossu.firebaseapp.com/",
+  },
+  {
+    title: "Píldoras informáticas",
+    type: "youtube",
+    description:
+      "Cursos gratuitos de desarrrollo de software en español de excelente calidad.",
+    link: "https://www.youtube.com/@pildorasinformaticas",
+  },
+];
