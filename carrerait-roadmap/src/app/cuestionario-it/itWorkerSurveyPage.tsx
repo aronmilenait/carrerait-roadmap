ed586d938f7ba48d783953d6eb1a029fc5ffeb33
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-26 21:15:55 -0300

    Add a page for the IT Worker Survey.

---
+++ carrerait-roadmap/src/app/cuestionario-it/itWorkerSurveyPage.tsx
@@ -0,0 +1,9 @@
+import ItWorkerSurvey from "../components/itWorkerSurvey";
+
+export default function About() {
+  return (
+    <main className="p-6 md:p-24">
+      <ItWorkerSurvey />
+    </main>
+  );
+}
