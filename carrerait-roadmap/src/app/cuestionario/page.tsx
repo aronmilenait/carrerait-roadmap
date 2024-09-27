1f6c315f94905aeda9f1ae60d03be22efda99e76
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-26 21:32:38 -0300

    Create the component SurveySelect and add it to the survey's page.

--- carrerait-roadmap/src/app/cuestionario/page.tsx
+++ carrerait-roadmap/src/app/cuestionario/page.tsx
@@ -1,9 +1,9 @@
-import Survey from "../components/Survey";
+import SurveySelect from "../components/SurveySelect";
 
 export default function About() {
   return (
     <main className="p-6 md:p-24">
-      <Survey />
+      <SurveySelect />
     </main>
   );
 }
