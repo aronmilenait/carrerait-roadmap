f503d49dde0ad11118a536e4ddae0068d1e8ba79
Milena Sol Aron <aronmilena.it@gmail.com>
2024-10-16 13:11:29 -0300

    Improve the appearance of the main orientation page.

--- carrerait-roadmap/src/app/orientacion/page.tsx
+++ carrerait-roadmap/src/app/orientacion/page.tsx
@@ -15,20 +15,22 @@ export default async function Orientation() {
 
   return (
     <main className="min-h-screen bg-gray-900 text-white p-4 sm:p-6 md:p-8 lg:p-24">
-      <h1 className="text-3xl sm:text-4xl md:text-5xl text-teal-400 font-extrabold mb-6 text-center">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl text-teal-400 font-extrabold mb-8 text-center tracking-wide">
         Guías y artículos de orientación
       </h1>
-      <div className="space-y-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {guidesMeta.map((guide) => (
           <Link key={guide.slug} href={`/orientacion/${guide.slug}`}>
-            <div className="bg-teal-800 hover:bg-teal-700 transition-colors duration-300 p-4 sm:p-6 rounded-lg shadow-lg cursor-pointer max-w-xl sm:max-w-2xl mx-auto mb-8">
-              <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-4">
+            <div className="bg-gray-800 p-4 sm:p-6 rounded-lg shadow-lg cursor-pointer hover:bg-teal-900 hover:shadow-2xl hover:scale-105 transition duration-300 ease-in-out">
+              <h2 className="text-2xl sm:text-2xl font-bold mb-2 sm:mb-4 text-teal-300">
                 {guide.title}
               </h2>
-              <p className="text-white mb-4 text-base sm:text-lg">
+              <p className="text-gray-300 mb-4 text-base sm:text-lg">
                 {guide.description}
               </p>
-              <div className="text-teal-50 text-sm sm:text-lg">Leer más...</div>
+              <div className="text-teal-50 text-sm sm:text-lg font-semibold">
+                Leer más...
+              </div>
             </div>
           </Link>
         ))}
