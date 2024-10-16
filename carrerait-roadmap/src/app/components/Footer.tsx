ffd16d6bc35a73823d81439007b61546124db461
Milena Sol Aron <aronmilena.it@gmail.com>
2024-10-16 12:23:57 -0300

    Create a constant for the link's classes.

--- carrerait-roadmap/src/app/components/Footer.tsx
+++ carrerait-roadmap/src/app/components/Footer.tsx
@@ -1,4 +1,7 @@
 const Footer = () => {
+  const footerLinksClasses =
+    "font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300";
+
   return (
     <footer className="bg-gray-900 py-6 px-4 sm:px-6 md:px-8 lg:px-16">
       <div className="max-w-4xl mx-auto text-center">
@@ -10,33 +13,21 @@ const Footer = () => {
           <span className="text-teal-500">IT</span>
         </a>
         <nav className="mt-4 flex justify-center space-x-6">
-          <a
-            href="/cuestionario"
-            className="font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300"
-          >
+          <a href="/cuestionario" className={footerLinksClasses}>
             Cuestionarios
           </a>
-          <a
-            href="/orientacion"
-            className="font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300"
-          >
+          <a href="/orientacion" className={footerLinksClasses}>
             Orientación
           </a>
-          <a
-            href="/recursos"
-            className="font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300"
-          >
+          <a href="/recursos" className={footerLinksClasses}>
             Recursos
           </a>
-          <a
-            href="/info"
-            className="font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300"
-          >
+          <a href="/info" className={footerLinksClasses}>
             Información
           </a>
           <a
             href="https://www.linkedin.com/in/milenasaron"
-            className="font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300"
+            className={footerLinksClasses}
             target="_blank"
             rel="noopener noreferrer"
           >
