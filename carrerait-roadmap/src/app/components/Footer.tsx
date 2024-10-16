21cebf7d4751154e04ce830f7ca89aaff45bf8e0
Milena Sol Aron <aronmilena.it@gmail.com>
2024-10-16 15:38:55 -0300

    Change background color.

--- carrerait-roadmap/src/app/components/Footer.tsx
+++ carrerait-roadmap/src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     "font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300";
 
   return (
-    <footer className="bg-gray-900 py-6 px-4 sm:px-6 md:px-8 lg:px-16">
+    <footer className="bg-gray-800 py-6 px-4 sm:px-6 md:px-8 lg:px-16">
       <div className="max-w-4xl mx-auto text-center">
         <a
           href="/"
