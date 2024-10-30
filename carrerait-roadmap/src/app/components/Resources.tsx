ce3d6292ea1b84b7a5d735b9b84ca2b9907757b1
Milena Sol Aron <aronmilena.it@gmail.com>
2024-10-30 18:25:47 -0300

    feat: add filtering by topic.

--- carrerait-roadmap/src/app/components/Resources.tsx
+++ carrerait-roadmap/src/app/components/Resources.tsx
@@ -1,6 +1,23 @@
+"use client";
+
+import { useState } from "react";
 import { resources } from "../data/resources";
 
 const Resources = () => {
+  const [selectedTopic, setSelectedTopic] = useState<string | null>(null);
+
+  const handleTopicSelection = (topic: string | null) => {
+    setSelectedTopic(topic);
+  };
+
+  const filteredResources = selectedTopic
+    ? resources.filter((resource) => resource.topic.includes(selectedTopic))
+    : resources;
+
+  const specificTopics = Array.from(
+    new Set(resources.flatMap((resource) => resource.topic))
+  );
+
   return (
     <section className="bg-gray-900 text-white py-10 px-4 max-w-3xl mx-auto">
       <h1 className="text-4xl sm:text-3xl md:text-5xl text-teal-400 font-extrabold mb-8 text-center">
@@ -9,8 +26,33 @@ const Resources = () => {
       <p className="text-2xl font-bold mb-8 text-center">
         Todos son gratuitos y de excelente calidad.
       </p>
+      <div className="mb-8 text-center flex flex-col justify-between">
+        <label
+          htmlFor="topic-select"
+          className="text-lg font-semibold text-teal-300 mr-2"
+        >
+          Filtrar por:
+        </label>
+        <select
+          id="topic-select"
+          onChange={(e) =>
+            handleTopicSelection(
+              e.target.value === "Todos" ? null : e.target.value
+            )
+          }
+          className="py-2 px-4 text-center rounded-md bg-teal-600 text-white"
+          value={selectedTopic || "Todos"}
+        >
+          <option value="Todos">Todos</option>
+          {specificTopics.map((topic) => (
+            <option key={topic} value={topic}>
+              {topic}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className="space-y-8">
-        {resources.map((resource, index) => (
+        {filteredResources.map((resource, index) => (
           <div
             key={index}
             className="p-6 bg-gray-800 rounded-lg shadow-lg border-l-4 border-teal-400 flex flex-col justify-between min-h-[200px]"
