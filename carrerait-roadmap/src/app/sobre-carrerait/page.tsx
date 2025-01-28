d6dc0208116c002661f2fa9f168d6690b5cf1f39
Milena Sol Aron <aronmilena.it@gmail.com>
2025-01-28 11:31:13 -0300

    refactor: remove padding

--- carrerait-roadmap/src/app/sobre-carrerait/page.tsx
+++ carrerait-roadmap/src/app/sobre-carrerait/page.tsx
@@ -3,9 +3,9 @@ import AboutWebsite from "../components/About/AboutWebsite";
 
 export default function AboutPage() {
   return (
-    <main className="p-6 md:p-24">
+    <>
       <AboutWebsite />
       <AboutMe />
-    </main>
+    </>
   );
 }
