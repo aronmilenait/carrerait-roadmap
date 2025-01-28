fbd836239d7036a2d811d533a9e4e28337503a67
Milena Sol Aron <aronmilena.it@gmail.com>
2025-01-28 11:31:04 -0300

    refactor: update about me

--- carrerait-roadmap/src/app/components/About/AboutMe.tsx
+++ carrerait-roadmap/src/app/components/About/AboutMe.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const AboutMe = () => {
   const paragraphClasses = [
@@ -11,96 +12,31 @@ const AboutMe = () => {
     "mb-6",
   ].join(" ");
 
-  const iconContainerClasses = "flex flex-col items-center space-y-2";
-
-  const iconClasses = [
-    "h-14",
-    "w-14",
-    "md:h-16",
-    "md:w-16",
-    "bg-gray-800",
-    "rounded-full",
-    "p-2",
-    "border-4",
-    "border-teal-700",
-    "text-teal-400",
-    "flex",
-    "items-center",
-    "justify-center",
-    "shadow-lg",
-    "hover:scale-110",
-    "transition-transform",
-    "duration-300",
-  ].join(" ");
-
-  const linkClasses =
-    "text-sky-300 hover:text-sky-400 transition-colors duration-300";
-
   return (
     <section className="bg-gray-900 text-white mx-auto rounded-lg p-8 sm:p-6 max-w-full sm:max-w-lg md:max-w-2xl">
       <h1 className="text-4xl sm:text-3xl md:text-5xl text-teal-400 font-extrabold text-sky-50 mb-6 text-center">
         Sobre mí
       </h1>
-      <div className="flex justify-center mb-8">
-      </div>
+      <div className="flex justify-center mb-4"></div>
       <p className={paragraphClasses}>
-        Soy Milena Sol Aron, la desarrolladora de este proyecto. Trabajo en el
-        rubro desde 2023, y comencé mi formación de manera autodidacta.
+        Soy{" "}
+        <a
+          href="https://www.linkedin.com/in/milenasaron/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-semibold text-teal-400 hover:text-teal-200 transition-colors duration-300"
+        >
+          Milena Sol Aron
+        </a>
+        , la desarrolladora de este proyecto. Trabajo en el rubro desde 2023, y
+        comencé mi formación de manera autodidacta.
       </p>
       <p className={paragraphClasses}>
-        Si tenés alguna duda o sugerencia, o simplemente querés hacer
-        networking, ¡no dudes en contactarme!
+        Me encargo de todo: el desarrollo de la plataforma mediante NextJS y
+        TypeScript, la escritura de los cuestionarios y guías de orientación, la
+        recopilación de recursos gratuitos, la difusión del proyecto, entre
+        otros.
       </p>
-      <div className="flex flex-wrap justify-center space-x-8 sm:space-x-6 md:space-x-10 mb-8">
-        <div className={iconContainerClasses}>
-          <a
-            href="https://github.com/aronmilenait"
-            className={linkClasses}
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="GitHub"
-          >
-            <div className={iconClasses}>
-              <FontAwesomeIcon icon={faGithub} className="h-7 w-7" />
-            </div>
-          </a>
-          <span className="text-xs md:text-lg font-semibold text-teal-300">
-            GitHub
-          </span>
-        </div>
-        <div className={iconContainerClasses}>
-          <a
-            href="https://www.linkedin.com/in/milenasaron"
-            className={linkClasses}
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="LinkedIn"
-          >
-            <div className={iconClasses}>
-              <FontAwesomeIcon icon={faLinkedin} className="h-7 w-7" />
-            </div>
-          </a>
-          <span className="text-xs md:text-lg font-semibold text-teal-300">
-            LinkedIn
-          </span>
-        </div>
-        <div className={iconContainerClasses}>
-          <a
-            href="mailto:aronmilena.it@gmail.com"
-            className={linkClasses}
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Email"
-          >
-            <div className={iconClasses}>
-              <FontAwesomeIcon icon={faEnvelope} className="h-7 w-7" />
-            </div>
-          </a>
-          <span className="text-xs md:text-lg font-semibold text-teal-300">
-            Email
-          </span>
-        </div>
-      </div>
     </section>
   );
 };
