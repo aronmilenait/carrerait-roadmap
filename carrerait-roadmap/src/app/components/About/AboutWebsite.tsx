e02ec2d433dbfd0070324d2000394bd71f4a1bb0
Milena Sol Aron <aronmilena.it@gmail.com>
2025-01-28 11:30:52 -0300

    refactor: update about website

--- carrerait-roadmap/src/app/components/About/AboutWebsite.tsx
+++ carrerait-roadmap/src/app/components/About/AboutWebsite.tsx
@@ -7,22 +7,6 @@ const AboutWebsite = () => {
     "mb-6",
   ].join(" ");
 
-  const listClasses = [
-    "list-disc",
-    "list-inside",
-    "text-left",
-    "text-base",
-    "md:text-lg",
-    "text-white",
-    "space-y-3",
-    "mb-6",
-    "font-semibold",
-    "border-teal-600",
-    "border-2",
-    "p-4",
-    "rounded-lg",
-  ].join(" ");
-
   return (
     <section className="bg-gray-900 text-white mx-auto rounded-lg p-8 sm:p-6 max-w-full sm:max-w-lg md:max-w-2xl">
       <h1 className="text-4xl sm:text-3xl md:text-5xl text-teal-400 font-extrabold text-sky-50 mb-6 text-center">
@@ -41,18 +25,10 @@ const AboutWebsite = () => {
       </p>
       <p className={paragraphClasses}>
         El objetivo de CarreraIT es ser un espacio para todos los que estamos
-        interesados o trabajando en IT.
-      </p>
-      <p className={paragraphClasses}>
-        Mediante cuestionarios divididos en base a tres factores importantes (si
-        estás trabajando en IT, si sos aspirante o si fuiste desvinculado), vas
-        a obtener recomendaciones personalizadas sobre cómo avanzar con tu
-        carrera.
-      </p>
-      <p className={paragraphClasses}>
-        Además de los cuestionarios, podés encontrar{" "}
-        <a href="/orientacion" className="font-bold">guías de orientación</a> y{" "}
-        <a href="/recursos" className="font-bold">recursos</a> gratuitos de excelente calidad.
+        interesados o trabajando en IT, repleto de recursos gratuitos de
+        excelente calidad, guías de orientación y cuestionarios con
+        recomendaciones personalizadas en base a tu situación laboral y
+        personal.
       </p>
     </section>
   );
