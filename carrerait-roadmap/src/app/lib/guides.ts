34f6b56ecf4a96d4fc79748b95d975bf1e96f36d
Milena Sol Aron <aronmilena.it@gmail.com>
2025-01-30 10:28:04 -0300

    refactor: move the types folder from app to src

--- carrerait-roadmap/src/app/lib/guides.ts
+++ carrerait-roadmap/src/app/lib/guides.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import { Guide, GuideMeta } from "../types/types";
+import { Guide, GuideMeta } from "../../types/types";
 import { remark } from "remark";
 import html from "remark-html";
 
--- carrerait-roadmap/src/app/orientacion/[slug]/page.tsx
+++ carrerait-roadmap/src/app/orientacion/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import { getGuideContent, getGuideSlugs } from "@/app/lib/guides";
-import { Guide } from "@/app/types/types";
+import { Guide } from "@/types/types";
 import { CornerUpLeft } from "lucide-react";
 import Link from "next/link";
 
