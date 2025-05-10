356e0affe150af85dc0cfaeff2dfa088a39b02a6
Milena Sol Aron <aronmilena.it@gmail.com>
2025-05-10 16:51:05 -0300

    feat: install clarity and remove vercel analytics

--- carrerait-roadmap/src/app/layout.tsx
+++ carrerait-roadmap/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,11 +37,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="ms_clarity">
+        {`
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "rhj89ui80h");
+        `}
+      </Script>
       <body className={`${inter.className}`}>
         <main className="min-h-screen grid grid-rows-[auto_1fr_auto] bg-gray-900 text-white">
           <Navbar />
           {children}
-          <Analytics />
+
           <Footer />
         </main>
       </body>
